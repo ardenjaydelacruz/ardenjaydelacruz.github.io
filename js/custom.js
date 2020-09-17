0ed4cbf522f7af07cb593de19ed1ddbf732f7b0c
Arden Jay Dela Cruz <ardenjaydelacruz@gmail.com>
2020-09-17 22:23:21 +0800

    first commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  AOS.init();
+  $(".button-collapse").sideNav({
+    menuWidth: 400, // Default is 240
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
+
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 1000
+  });
+});
