65807159debf96344d3b607246ca3a837ab4fa1e
Arden Jay Dela Cruz <ardenjaydelacruz@gmail.com>
2020-09-23 12:14:43 +0800

    Fixed based on review

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   AOS.init();
   $(".button-collapse").sideNav({
-    menuWidth: 400, // Default is 240
+    menuWidth: 600, // Default is 240
     edge: 'left', // Choose the horizontal origin
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
   });
